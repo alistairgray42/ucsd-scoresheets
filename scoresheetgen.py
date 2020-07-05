0d6ca93d685d9fac412991b804a4945ac45e559c
Alistair Gray <alistairgray42@gmail.com>
2020-07-04 18:07:19 -0700

    scoresheet generation is complete and correct

--- scoresheetgen.py
+++ scoresheetgen.py
@@ -3,6 +3,8 @@ import httplib2
 import os
 import pickle
 import string
+import sys
+import json
 
 from collections import OrderedDict
 
@@ -11,6 +13,8 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 from copy import copy
+
+from utils import send_completion_email, generate_filename
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/sheets.googleapis.com-python-quickstart.json
@@ -31,19 +35,7 @@ def get_gridRange(A1, i):
     return {"sheetId": i, "startRowIndex": row - 1, "endRowIndex": row, "startColumnIndex": d[col], "endColumnIndex": d[col] + 1}
 
 
-def get_rooms():
-    d = OrderedDict()
-    while True:
-        inp = input("room number: ")
-        if(len(inp) == 0):
-            break
-        else:
-            d[inp] = ""
-    return d
-
-
 # from Google Sheets API v4 Python quickstart
-
 def get_credentials(filename):
     creds = None
     # The file  stores the user's access and refresh tokens, and is
@@ -67,17 +59,19 @@ def get_credentials(filename):
 
 
 class ScoresheetGenerator:
-    def __init__(self, *, checkboxes=False, tournament_name, scoresheet_id, aggregate_id, roster_id, room_names):
+    def __init__(self, *, checkboxes=False, tournament_name, email, room_names):
         self.tournament_name = tournament_name
-        self.scoresheet_id = scoresheet_id
-        self.aggregate_id = aggregate_id
-        self.roster_id = roster_id
+        self.email = email
         self.room_names = room_names
         self.room_ids = dict()
 
         self.sheet_names = ["Round " + str(i + 1) for i in range(15)]
         self.A_PLAYERS = ["C3", "D3", "E3", "F3", "G3", "H3"]
         self.B_PLAYERS = ["M3", "N3", "O3", "P3", "Q3", "R3"]
+
+        # TEMPORARY! Templates!
+        self.roster_id = "1k8H0kUeE3gwNpZjyIq8cUbEatwNzA-NNGWdS8YdiFeU"
+        self.aggregate_id = "1uvBPrEUoFNadWTIzIYfjtCwo328vPangAw_AXjGtOg4"
 
         if checkboxes:
             self.team_a_range = "{}!C1:H3"
@@ -95,6 +89,9 @@ class ScoresheetGenerator:
             self.TEAM_A_VAL = "AC"
             self.TEAM_B_VAL = "AD"
 
+            # TEMPORARY! Template!
+            self.scoresheet_id = "1_XuTrn4cmxE1IxGzOkc8l-VqMaE-vn1eWzwv8rs1lyE"
+
         else:
             self.team_a_range = "{}!C1:H3"
             self.team_b_range = "{}!M1:R3"
@@ -111,24 +108,21 @@ class ScoresheetGenerator:
             self.TEAM_A_VAL = "Y"
             self.TEAM_B_VAL = "Z"
 
+            # TEMPORARY! Template!
+            self.scoresheet_id = "1pv0Z5kvxfij0KCRAQG9lOxfaax-HftmGnDxQn59SDPY"
+
         self.left_col = ["=CONCAT(\"A BP: \",IMPORTRANGE(\"{}\",\"{}!I32\"))", "=CONCAT(\"B BP: \",IMPORTRANGE(\"{}\",\"{}!U32\"))",
                          "TUH", "15", "10", "-5", "Total", "=IMPORTRANGE(\"{}\",\"{}!AA1\")"]
         self.importrange_fstring = "={{IMPORTRANGE(\"{}\",\"{}\"),IMPORTRANGE(\"{}\",\"{}\")}}"
 
-    def generate(self):
-        self.create_scoresheets()
-        self.populate_aggregate()
-        self.populate_rooms()
-        self.rosters()
-
     def create_scoresheets(self):
         # set up APIs
         driveService = discovery.build('drive', 'v3', credentials=get_credentials(DRIVE_CRED_FILE))
+        self.driveService = driveService
 
         credentials = get_credentials(DRIVE_CRED_FILE)
         discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?version=v4')
-        service = discovery.build(
-            'sheets', 'v4', credentials=credentials, discoveryServiceUrl=discoveryUrl)
+        self.service = discovery.build('sheets', 'v4', credentials=credentials, discoveryServiceUrl=discoveryUrl)
 
         # create a new folder to move all sheets to
         file_metadata = {
@@ -137,158 +131,195 @@ class ScoresheetGenerator:
         }
 
         file = driveService.files().create(body=file_metadata, fields='id').execute()
-        folder_id = file.get('id')
-
+        self.folder_id = file.get('id')
         # create aggregate sheet, roster sheet, room sheets
-        file_metadata = {"name": "Aggregate Scoresheets", "parents": [folder_id]}
+        file_metadata = {"name": "Aggregate Scoresheets",
+                         "parents": [self.folder_id]}
         c = driveService.files().copy(fileId=self.aggregate_id, body=file_metadata).execute()
-        self.aggregate_id = c["id"]  # update in place with ID's of copies
+        self.aggregate_id = c["id"]  # update ID's in place with ID's of copies
 
-        file_metadata = {"name": "Rosters", "parents": [folder_id]}
+        file_metadata = {"name": "Rosters", "parents": [self.folder_id]}
         c = driveService.files().copy(fileId=self.roster_id, body=file_metadata).execute()
         self.roster_id = c["id"]
 
         for room in self.room_names:
-            file_metadata = {"name": room, "parents": [folder_id]}
-            c = driveService.files().copy(fileId=self.scoresheet_id, body=file_metadata).execute()
+            file_metadata = {"name": room, "parents": [self.folder_id]}
+            c = driveService.files().copy(fileId=self.scoresheet_id,
+                                          body=file_metadata).execute()
             self.room_ids[room] = c["id"]
 
-        # print("Created room sheets")
-
     def populate_aggregate(self):
+        data = []
+
         for sheet_name in self.sheet_names:
             i = 1
             for room in self.room_ids:
-                left_col_fmted = copy(left_col)
+                left_col_fmted = copy(self.left_col)
                 left_col_fmted.insert(0, room)
                 left_col_fmted[1] = left_col_fmted[1].format(self.room_ids[room], sheet_name)
                 left_col_fmted[2] = left_col_fmted[2].format(self.room_ids[room], sheet_name)
                 left_col_fmted[8] = left_col_fmted[8].format(self.room_ids[room], sheet_name)
 
-                data = [{"range": sheet_name+"!A{}:A{}".format(i, i+9), "values": [left_col_fmted]},
-                        {"range": sheet_name+"!B{}".format(i),
-                         "values": [[importrange_fstring.format(self.room_ids[room], team_a_range.format(sheet_name), self.room_ids[room], team_b_range.format(sheet_name))]]},
-                        {"range": sheet_name+"!B{}".format(i+3),
-                         "values": [[importrange_fstring.format(self.room_ids[room], indiv_a_range.format(sheet_name), self.room_ids[room], indiv_b_range.format(sheet_name))]]}]
+                data.append({"range": f"{sheet_name}!A{i}:A{i + 9}", "values": [left_col_fmted]})
 
-                data.append({"range": sheet_name + "!B{}".format(i+8),
+                data.append({"range": f"{sheet_name}!B{i}",
+                             "values": [[self.importrange_fstring.format(self.room_ids[room], self.team_a_range.format(sheet_name),
+                                                                         self.room_ids[room], self.team_b_range.format(sheet_name))]]})
+
+                data.append({"range": f"{sheet_name}!B{i + 3}",
+                             "values": [[self.importrange_fstring.format(self.room_ids[room], self.indiv_a_range.format(sheet_name),
+                                                                         self.room_ids[room], self.indiv_b_range.format(sheet_name))]]})
+
+                data.append({"range": f"{sheet_name}!B{i + 8}",
                              "values": [["https://docs.google.com/spreadsheets/d/{}/edit".format(self.room_ids[room])]]})
-                d = {}
-                for j in data:
-                    j["majorDimension"] = "COLUMNS"
-                d["data"] = data
-                d["valueInputOption"] = "USER_ENTERED"
 
-                res = service.spreadsheets().values().batchUpdate(spreadsheetId=agg_id, body=d).execute()
                 i += 10
-            print("Finished: " + sheet_name)
-        print("Finished aggregation")
 
+        d = {}
+        for j in data:
+            j["majorDimension"] = "COLUMNS"
+        d["data"] = data
+        d["valueInputOption"] = "USER_ENTERED"
+
+        res = self.service.spreadsheets().values().batchUpdate(spreadsheetId=self.aggregate_id, body=d).execute()
 
     def populate_rooms(self):
-        data = [{"range": "Rosters!A1", "values": [
-            ["=IMPORTRANGE(\"{}\", \"Rosters!A:G\")".format(master_roster_id)]]}]
+        data = [{"range": "Rosters!A1", "values": [["=IMPORTRANGE(\"{}\", \"Rosters!A:G\")".format(self.roster_id)]]}]
 
-        for s in sheet_names:
+        for s in self.sheet_names:
             data.append({"range": "{}!{}".format(s, self.A_ROSTERS), "values": [
                         ["=TRANSPOSE(FILTER(Rosters!B:G, Rosters!A:A = {}))".format(self.TEAM_A)]]})
             data.append({"range": "{}!{}".format(s, self.B_ROSTERS), "values": [
                         ["=TRANSPOSE(FILTER(Rosters!B:G, Rosters!A:A = {}))".format(self.TEAM_B)]]})
             data.append({"range": "{}!{}".format(s, self.TEAMS),
-                        "values": [["=ARRAYFORMULA(Rosters!A:A)"]]})
+                         "values": [["=ARRAYFORMULA(Rosters!A:A)"]]})
 
         d = {}
         for j in data:
             j["majorDimension"] = "COLUMNS"
-            d["data"] = data
-            d["valueInputOption"] = "USER_ENTERED"
-            for i in self.room_ids:
-                res = service.spreadsheets().values().batchUpdate(spreadsheetId=self.room_ids[i], body=d).execute()
-            print(j)
-
-        sheetIds = {}
+        d["data"] = data
+        d["valueInputOption"] = "USER_ENTERED"
         for i in self.room_ids:
-            a = []
-            b = service.spreadsheets().get(spreadsheetId=self.room_ids[i]).execute()
-            sheetIds[i] = [j["properties"]["sheetId"] for j in b["sheets"]]
+            res = self.service.spreadsheets().values().batchUpdate(spreadsheetId=self.room_ids[i], body=d).execute()
 
+        self.sheetIds = {}
+        for i in self.room_ids:
+            b = self.service.spreadsheets().get(spreadsheetId=self.room_ids[i]).execute()
+            self.sheetIds[i] = [j["properties"]["sheetId"] for j in b["sheets"]]
 
     def rosters(self):
         for i in self.room_ids:
             request = []
             k = 0
 
-            for sheetname in sheet_names:
+            for sheetname in self.sheet_names:
                 for A in self.A_PLAYERS:
                     dropdown_action = {
                         'setDataValidation': {
-                            'range': get_gridRange(A, sheetIds[i][k]),
+                            'range': get_gridRange(A, self.sheetIds[i][k]),
                             'rule': {
                                 'condition': {
                                     'type': 'ONE_OF_RANGE',
                                     'values': [
-                                        # { "userEnteredValue" : "=Y:Y" }
-                                        {"userEnteredValue": "={}:{}".format(TEAM_A_VAL)}
+                                        {"userEnteredValue": "={}:{}".format(
+                                            self.TEAM_A_VAL, self.TEAM_A_VAL)}
                                     ],
                                 },
                                 'inputMessage': 'Choose one from dropdown',
                                 'showCustomUi': True
-                            } } }
+                            }}}
                     request.append(dropdown_action)
 
                 for B in self.B_PLAYERS:
                     dropdown_action = {
                         'setDataValidation': {
-                            'range': get_gridRange(B, sheetIds[i][k]),
+                            'range': get_gridRange(B, self.sheetIds[i][k]),
                             'rule': {
                                 'condition': {
                                     'type': 'ONE_OF_RANGE',
                                     'values': [
-                                        # { "userEnteredValue" : "=Z:Z" }
-                                        {"userEnteredValue": "={}:{}".format(TEAM_B_VAL)}
+                                        {"userEnteredValue": "={}:{}".format(
+                                            self.TEAM_B_VAL, self.TEAM_B_VAL)}
                                     ],
                                 },
                                 'inputMessage': 'Choose one from dropdown',
                                 'showCustomUi': True
-                            } } }
+                            }}}
                     request.append(dropdown_action)
 
-                    dropdown_action = {
-                        'setDataValidation': {
-                            'range': get_gridRange(self.TEAM_A, sheetIds[i][k]),
-                            'rule': {
-                                'condition': {
-                                    'type': 'ONE_OF_RANGE',
-                                    'values': [
-                                        # { "userEnteredValue" : "=X:X" }
-                                        {"userEnteredValue": "={}:{}".format(TEAM_VAL)}
-                                    ],
-                                },
-                                'inputMessage': 'Choose one from dropdown',
-                                'showCustomUi': True
-                            } } }
-                    request.append(dropdown_action)
+                dropdown_action = {
+                    'setDataValidation': {
+                        'range': get_gridRange(self.TEAM_A, self.sheetIds[i][k]),
+                        'rule': {
+                            'condition': {
+                                'type': 'ONE_OF_RANGE',
+                                'values': [
+                                    {"userEnteredValue": "={}:{}".format(
+                                        self.TEAM_VAL, self.TEAM_VAL)}
+                                ],
+                            },
+                            'inputMessage': 'Choose one from dropdown',
+                            'showCustomUi': True
+                        }}}
+                request.append(dropdown_action)
 
-                    dropdown_action = {
-                        'setDataValidation': {
-                            'range': get_gridRange(self.TEAM_B, sheetIds[i][k]),
-                            'rule': {
-                                'condition': {
-                                    'type': 'ONE_OF_RANGE',
-                                    'values': [
-                                        # { "userEnteredValue" : "=X:X" }
-                                        {"userEnteredValue": "={}:{}".format(TEAM_VAL)}
-                                    ],
-                                },
-                                'inputMessage': 'Choose one from dropdown',
-                                'showCustomUi': True
-                            } } }
-                    request.append(dropdown_action)
-                    k += 1
+                dropdown_action = {
+                    'setDataValidation': {
+                        'range': get_gridRange(self.TEAM_B, self.sheetIds[i][k]),
+                        'rule': {
+                            'condition': {
+                                'type': 'ONE_OF_RANGE',
+                                'values': [
+                                    {"userEnteredValue": "={}:{}".format(
+                                        self.TEAM_VAL, self.TEAM_VAL)}
+                                ],
+                            },
+                            'inputMessage': 'Choose one from dropdown',
+                            'showCustomUi': True
+                        }}}
+                request.append(dropdown_action)
 
-            # print(request)
+                k += 1
+
             batchUpdateRequest = {'requests': request}
-            service.spreadsheets().batchUpdate(spreadsheetId=self.room_ids[i], body=batchUpdateRequest).execute()
+            self.service.spreadsheets().batchUpdate(spreadsheetId=self.room_ids[i], body=batchUpdateRequest).execute()
 
-            print("Finished Rosters")
+    def generate(self):
+        self.create_scoresheets()
+        self.populate_aggregate()
+        self.populate_rooms()
+        self.rosters()
 
+    def share_with_recipient(self):
+        permission = {
+            'type': 'user',
+            'role': 'writer',
+            'emailAddress': self.email
+        }
+
+        SHARE_MESSAGE = """Thanks for using UCSD Scoresheets!
+
+        This is the folder containing your scoresheets; make sure to go into each sheet and connect them as necessary. (Check the website if you don't know how to do this.)
+        """
+
+        self.driveService.permissions().create(fileId=self.folder_id, body=permission, emailMessage=SHARE_MESSAGE).execute()
+
+    # TODO: use a proper database for all this
+    def write_config(self):
+        with open(os.path.join("sqbs_configs", generate_filename(self.email, ".json")), 'w') as f:
+            json.dump({"agg_id": self.aggregate_id,
+                       "roster_id": self.roster_id,
+                       "last_run": 0}, f)
+
+
+j = json.load(open(sys.argv[1], 'r'))
+sg = ScoresheetGenerator(
+    checkboxes=(j["bonus_tracking"] == "on"),
+    tournament_name=j["tourney_name"],
+    email=j["email"],
+    room_names=j["rooms"])
+
+sg.generate()
+sg.share_with_recipient()
+sg.write_config()
+# send_completion_email(sg.email, sg.aggregate_id)
